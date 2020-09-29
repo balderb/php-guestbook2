97fe9f2afc7e4f8c090615eddddf5cffdd67c9a4
Balder Bongaerts <59473814+balderb@users.noreply.github.com>
2020-09-29 19:26:31 +0200

    changing models dir name to model

--- index.php
+++ index.php
@@ -1,87 +1,78 @@
 <?php
+
 declare(strict_types=1);
+//require "model/autoload.php";
+require "model/Subject.php";
+require "model/Message.php";
+require "model/Visitor.php";
+require "model/GuestBook.php";
 
-// Start the session
 
-session_start();
+$guestBook = new GuestBook();
+$errMessage="";
 
-    if(isset($_POST['post'])) {
-    $title = $_POST['$title'];
-    $date = $_POST['$date'];
-    $content = $_POST['$content'];
-    $author = $_POST['$author'];
+
+if ($_SERVER["REQUEST_METHOD"]=="POST" ){
+// validating
+//validate author
+    if (empty($_POST["name"]))
+    {  $errMessage=$errMessage.", Author is empty";
+    }else{
+        $name=cleanInput($_POST["name"]);
+    }
+
+//Validate e-mail
+    if (empty($_POST["email"])){
+        $errMessage=$errMessage."e-mail is empty !";
+    }else{
+        $email=cleanInput($_POST["email"]);
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $errMessage=$errMessage."Invalidate email !";
+        }
+    }
+
+//Validating title
+    if (empty($_POST["title"])){
+        $errMessage=$errMessage.", Title is empty";
+    }else{
+        $title=cleanInput($_POST["title"]);
+    }
+
+//Validate content
+    if (empty($_POST["content"])){
+        $errMessage=$errMessage.", Content is empty";
+
+    }else{
+
+        $content=cleanInput($_POST["content"]);
+    }
+
+    if (!empty($errMessage)){
+        $errMessage="Please check and fix this issues :) : ".$errMessage;
+        $errMessage= " <div class='alert alert-dismissible alert-danger'>
+<h4 class='alert-heading'>Warning!</h4>
+<p class='mb-0'>$errMessage
+</p> </div>";
+
+        $userMessage=$errMessage;
+//    return false;
+    } else{
+// return true;
+        $author=new Visitor($name,$email);
+        $newPost= new Message($author,$title,$_POST["content"]);
+        $guestBook->savePost($newPost);
+    }
+
+
 }
 
+
+function cleanInput($data){
+    $data=trim($data);
+    $data=stripslashes($data);
+    $data=htmlspecialchars($data);
+    return $data;
+};
+
+require 'view/guestBookView.php';
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Guest-book challenge</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-</head>
-<body>
-
-<div class = "main container">
-    <header>
-        <h1>Welcome to the Guestbook challenge</h1>
-        <p>Please leave a comment below. It's free! :)</p>
-
-    </header>
-
-    <main>
-        <form action="#" method="post">
-
-            <label for="title"> Title:
-            <input type="text" name="title"/>
-            </label>
-
-            <br>
-
-            <label for="date"> Date of today:
-            <input type="date" name="date" required pattern="\d{4}-\d{2}-\d{2}"/>
-            </label>
-
-            <br>
-
-            <label for="content"> Your message:
-            <input type="text" name="content"/>
-            </label>
-
-            <br>
-
-            <label for="author"> Your name:
-            <input type="text" name="author"/>
-            </label>
-
-            <br>
-
-            <label for="post"> Send it:
-            <button type="submit" name="post">Ready<button/>
-            </label>
-
-        </form>
-
-        <div>
-            <?php echo $guestBook->getMessages(); ?>
-        </div>
-
-    </main>
-
-    <footer>
-        <!–– task here comes the function call getFooter() in php as indicated in Tips & Advice ––>
-    </footer>
-
-</div>
-
-</body>
-</html>
-
-
-
-
-
-
-
---
+++ tester.php
@@ -0,0 +1,87 @@
+<?php
+declare(strict_types=1);
+
+// Start the session
+
+session_start();
+
+    if(isset($_POST['post'])) {
+    $title = $_POST['$title'];
+    $date = $_POST['$date'];
+    $content = $_POST['$content'];
+    $author = $_POST['$author'];
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Guest-book challenge</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class = "main container">
+    <header>
+        <h1>Welcome to the Guestbook challenge</h1>
+        <p>Please leave a comment below. It's free! :)</p>
+
+    </header>
+
+    <main>
+        <form action="#" method="post">
+
+            <label for="title"> Title:
+            <input type="text" name="title"/>
+            </label>
+
+            <br>
+
+            <label for="date"> Date of today:
+            <input type="date" name="date" required pattern="\d{4}-\d{2}-\d{2}"/>
+            </label>
+
+            <br>
+
+            <label for="content"> Your message:
+            <input type="text" name="content"/>
+            </label>
+
+            <br>
+
+            <label for="author"> Your name:
+            <input type="text" name="author"/>
+            </label>
+
+            <br>
+
+            <label for="post"> Send it:
+            <button type="submit" name="post">Ready<button/>
+            </label>
+
+        </form>
+
+        <div>
+            <?php echo $guestBook->getMessages(); ?>
+        </div>
+
+    </main>
+
+    <footer>
+        <!–– task here comes the function call getFooter() in php as indicated in Tips & Advice ––>
+    </footer>
+
+</div>
+
+</body>
+</html>
+
+
+
+
+
+
+
